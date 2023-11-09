8dac8c84fcd61083e3b88f5881f8bff145016762
greensgit <rajgreensomr@gmail.com>
2023-11-09 13:27:49 +0530

    new by selva

--- src/org/cts/test/Test.java
+++ src/org/cts/test/Test.java
@@ -4,5 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("this is test");
 	}
-
+	
+	private void cancel() {
+		System.out.println("This is selva");
+	}
 }
